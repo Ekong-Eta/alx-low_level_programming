01fb15a78dd29098318f2953c5383e8ac7edbd12
Ekong-Eta <etaekong55@gmail.com>
2023-02-21 23:11:28 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - Prints the size of various type on computer it is compiled and run on..
+ * main - Prints the size of various types on computer it is compiled
+ * and run on..
  * Return: Always 0.
  */
 int main(void)
