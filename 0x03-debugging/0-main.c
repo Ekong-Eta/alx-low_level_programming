d80d8af4eaf4fcbf7da60fc440e1979e77b56f2a
Ekong-Eta <etaekong55@gmail.com>
2023-02-28 02:10:41 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * main - tests function that prints if integer is positive or negative
-* Return: 0 (Success)
+* Return:Always 0 (Success)
 */
 int main(void)
 {
@@ -9,6 +9,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
