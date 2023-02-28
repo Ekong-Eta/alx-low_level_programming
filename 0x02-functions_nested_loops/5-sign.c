ab7b4280781fb6c590e9fc53a2ef5b6b036f7e03
Ekong-Eta <etaekong55@gmail.com>
2023-02-28 07:38:32 -0800

    6-abs.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,26 @@
 
 /**
  * print_sign - Prints the sign of a number
+ *
  * @n: The number to be checked
  * Retrun: 1 for positive num, -1 for negative num or zero for something else
  */
 
 int print_sign(int n)
 {
-	if (n >= 0)
+	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar(45);
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		_putchar(48);
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
-	return (0);
 }
